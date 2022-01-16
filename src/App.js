ab7b0265cdf857600ad06fb0c0bf554edcfcc0da
nata <73900609+nataccc@users.noreply.github.com>
2022-01-16 14:58:33 +0700

    oi

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import "./App.css";
+import "./Assets/App.scss";
+
+function App() {
+  let data1 = 0;
+  const data2 = () => {};
+  function name(params) {}
+
+  return (
+    <div className="container">
+      
+      <div className='navbar'>
+        header
+      </div>
+
+      <div className="Selection">
+      <div className="Card Selection-body-1">1</div>
+      <div className="Card Selection-body-2">2</div>
+      <div className="Card Selection-body-3">3</div>
+      <div className="Card Selection-body-4">4</div>
+      <div className="Card Selection-body-5">5</div>
+      <div className="Card Selection-body-6">6</div>
+      </div>
+
+      <div className='koplo'>
+        <div className='koplo-1'>1</div>
+        <div className='koplo-2'>2</div>
+        </div>
+      
+      <div className='netnot'>
+        <div className='netnot-1'>Awokwowkowk</div>
+        <div className='netnot-2'>2</div>
+        <div className='netnot-3'>3</div>
+        </div>
+
+
+      
+      
+      </div>
+  );
+}
+
+export default App;
